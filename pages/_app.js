00a8cbaf68934c7a6be73a64ebde9202a6c2a01f
Yusuf Mert <ymert60@hotmail.com>
2022-08-19 00:04:00 +0300

    gtag test 2

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { ClinicsContextProvider } from "./../store/clinics-context";
 import { ContextProvider } from "../store/context";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
-
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import React from "react";
@@ -13,18 +12,28 @@ config.autoAddCss = false;
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-
-
   return (
-      <ContextProvider>
-        <ClinicsContextProvider>
-          <Layout>
-            <GoogleAnalytics/>
-            <Component {...pageProps} />
-          </Layout>
-        </ClinicsContextProvider>
-      </ContextProvider>
- 
+    <ContextProvider>
+      <ClinicsContextProvider>
+        <Layout>
+          {/* 
+          <GoogleAnalytics /> */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-HW23HL84Q5`}
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-HW23HL84Q5');
+    `}
+          </Script>
+          <Component {...pageProps} />
+        </Layout>
+      </ClinicsContextProvider>
+    </ContextProvider>
   );
 }
 
