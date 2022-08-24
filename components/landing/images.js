2ea2d50e8fb65c41be89c43881094127e7c7ee18
Yusuf Mert <ymert60@hotmail.com>
2022-08-25 02:35:39 +0300

    errors fixed

--- components/landing/images.js
+++ components/landing/images.js
@@ -2,56 +2,56 @@ import Image from "next/image";
 
 export default function Images() {
   return (
-    <section class="overflow-hidden text-gray-700 px-4 py-24">
+    <section className="overflow-hidden text-gray-700 px-4 py-24">
       <h2 className="text-lg text-center">
         Get your operation done by the best surgeons in Istanbul and enjoy
-        Istanbul's views, food and culture!
+        Istanbul&apos;s views, food and culture!
       </h2>
-      <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
-        <div class="flex flex-wrap -m-1 md:-m-2">
-          <div class="flex flex-wrap w-1/2">
-            <div class="w-1/2 p-1 md:p-2">
+      <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+        <div className="flex flex-wrap -m-1 md:-m-2">
+          <div className="flex flex-wrap w-1/2">
+            <div className="w-1/2 p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
+                className="block object-cover object-center w-full h-full rounded-lg"
                 src="/images/landing/images-section/avicenna-atasehir.png"
               />
             </div>
-            <div class="w-1/2 p-1 md:p-2">
+            <div className="w-1/2 p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
+                className="block object-cover object-center w-full h-full rounded-lg"
                 src="/images/landing/images-section/atasehir.png"
               />
             </div>
-            <div class="w-full p-1 md:p-2">
+            <div className="w-full p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
+                className="block object-cover object-center w-full h-full rounded-lg"
                 src="/images/landing/images-section/istanbul2.jpg"
               />
             </div>
           </div>
-          <div class="flex flex-wrap w-1/2">
-            <div class="w-full p-1 md:p-2">
+          <div className="flex flex-wrap w-1/2">
+            <div className="w-full p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
+                className="block object-cover object-center w-full h-full rounded-lg"
                 src="/images/landing/images-section/istanbul.jfif"
               />
             </div>
-            <div class="w-1/2 p-1 md:p-2">
+            <div className="w-1/2 p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+                className="block object-cover object-center w-full h-full rounded-lg"
+                src="/images/landing/images-section/plastic-surgeries.jpg"
               />
             </div>
-            <div class="w-1/2 p-1 md:p-2">
+            <div className="w-1/2 p-1 md:p-2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full rounded-lg"
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+                className="block object-cover object-center w-full h-full rounded-lg"
+                src="/images/landing/images-section/quality.png"
               />
             </div>
           </div>
--- components/procedures/procedures.js
+++ components/procedures/procedures.js
@@ -11,42 +11,42 @@ export default function Procedures(props) {
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xxl:grid-cols-5">
         {procedures.map((procedure) => {
           return (
-            <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md ">
+            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md " key={procedure.slug}>
               <a href="#">
                 <Image
-                  class="rounded-t-lg"
+                  className="rounded-t-lg"
                   src={procedure.image}
                   alt=""
                   width={350}
                   height={200}
                 />
               </a>
-              <div class="p-5">
+              <div className="p-5">
                 <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight ">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight ">
                     {procedure.title}
                   </h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                   {procedure.description}
                 </p>
                 <Link href={`/procedures/${procedure.slug}`}>
                   <a
                     href="#"
-                    class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-brightPrimary rounded-lg hover:bg-darkPrimary focus:ring-4 focus:outline-none focus:ring-purple-300"
+                    className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-brightPrimary rounded-lg hover:bg-darkPrimary focus:ring-4 focus:outline-none focus:ring-purple-300"
                   >
                     Find Clinics
                     <svg
                       aria-hidden="true"
-                      class="ml-2 -mr-1 w-4 h-4"
+                      className="ml-2 -mr-1 w-4 h-4"
                       fill="currentColor"
-                      viewBox="0 0 20 20"
+                      viewBox="0 0 20 20"               
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </a>
